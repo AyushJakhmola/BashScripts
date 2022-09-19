6860d9e8b2625abfe3ae28bee16ced3e370cccf0
AyushJakhmola <ayushjakhmola001@gmail.com>
2022-09-19 10:46:46 +0530

    bacup mysql

---
+++ mysqlbackup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+systemctl status mysql.service | grep 'active' > /dev/null 2>&1
+if [ $? != 0 ];
+then
+    echo "mysql service is stopped"
+    else
+    echo "mysql service is running"
+    systemctl stop mysql.service
+fi        
+    sudo mysqldump -u root --all-databases > "$(date +%d-%m-20%y,Time=%T).sql"
+    aws s3 cp "$(date +%d-%m-20%y,Time=%T).sql" s3://mydb-database-backup/
