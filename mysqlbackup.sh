e1fb13bc1c3e15dc94278bcef367cbfe119b7e3b
Ayush Jakhmola <ayushjakhmola001@gmail.com>
2022-09-23 10:50:20 +0530

    Update mysqlbackup.sh

--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -9,3 +9,4 @@ then
 fi        
     sudo mysqldump -u root --all-databases > "$(date +%d-%m-20%y,Time=%T).sql"
     aws s3 cp "$(date +%d-%m-20%y,Time=%T).sql" s3://mydb-database-backup/
+    systemctl starp mysql.service
